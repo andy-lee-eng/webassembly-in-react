71b91370e278121a0657b77a341112dfdf2f991c
Andy <alee@scottlogic.com>
2019-06-13 13:52:23 +0100

    Implemented factorial function

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,5 +1,5 @@
 // The entry file of your WebAssembly module.
-
-export function add(a: i32, b: i32): i32 {
-  return a + b;
+export function factorial(value: f64): f64 {
+  if (value == 0 || value == 1) return 1;
+  return value * factorial(value - 1);
 }
