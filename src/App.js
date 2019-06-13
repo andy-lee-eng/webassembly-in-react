4f5a9631d7ea41b62d7f84c4a3ee9e2d4c9a2d8b
Andy <alee@scottlogic.com>
2019-06-13 13:53:42 +0100

    Add simple form to start page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+import waApi from './wa-api';
+
 function App() {
+  const [value, setValue] = useState(5);
+  const [result, setResult] = useState();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        The factorial of
+        <input value={value} onChange={evt => setValue(evt.target.value)} />
+        is {result}
+      </p>
+      <button onClick={async () => setResult((await waApi).factorial(value))}>
+        Calculate
+      </button>
     </div>
   );
 }
